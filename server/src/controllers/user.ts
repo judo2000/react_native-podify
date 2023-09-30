473b142cfddd2c98dd86c69e96c2e06347a9f077
Stephen Moore <judo2000@comcast.net>
2023-09-30 14:37:45 -0600

    Refacter and create mail helper

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,59 +1,24 @@
 import { RequestHandler } from "express";
-import nodemailer from "nodemailer";
-import path from "path";
 
 import User from "#/models/user";
-import emailVerificationToken from "#/models/emailVerificationToken";
 import { CreateUser } from "#/@types/user";
-import { MAILTRAP_USER, MAILTRAP_PASS } from "#/utils/variables";
 import { generateToken } from "#/utils/helper";
-import { generateTemplate } from "#/mail/template";
+import { sendVerificationEmail } from "#/utils/mail";
+
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { name, email, password } = req.body;
 
   const user = await User.create({ name, email, password });
 
+  // send verification email
   const token = generateToken();
-  await emailVerificationToken.create({
-    owner: user._id,
-    token,
-  });
+  sendVerificationEmail(token, { name, email, userId: user._id.toString() });
 
-  const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: MAILTRAP_USER,
-      pass: MAILTRAP_PASS,
+  res.status(201).json({
+    user: {
+      id: user._id,
+      name,
+      email,
     },
   });
-
-  const welcomeMessage = `Hi ${name}, welcome to Podify!  There are so many things we do for verified users.  Use the given OTP (One-Time-Password) to verify your email.`;
-
-  transport.sendMail({
-    to: user.email,
-    from: "auth@myapp.com",
-    subject: "Welcome to Podify",
-    html: generateTemplate({
-      title: "Welcome to Podify",
-      message: welcomeMessage,
-      logo: "cid:logo",
-      banner: "cid:welcome",
-      link: "#",
-      btnTitle: token,
-    }),
-    attachments: [
-      {
-        filename: "logo.png",
-        path: path.join(__dirname, "../mail/logo.png"),
-        cid: "logo",
-      },
-      {
-        filename: "welcome.png",
-        path: path.join(__dirname, "../mail/welcome.png"),
-        cid: "welcome",
-      },
-    ],
-  });
-  res.status(201).json({ user });
 };
