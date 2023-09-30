f40110c66f7a3b092e4072c7de4a63dfd7697bed
Stephen Moore <judo2000@comcast.net>
2023-09-30 11:12:50 -0600

    Generate and send email verification token

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -2,14 +2,21 @@ import { RequestHandler } from "express";
 import nodemailer from "nodemailer";
 
 import User from "#/models/user";
+import emailVerificationToken from "#/models/emailVerificationToken";
 import { CreateUser } from "#/@types/user";
 import { MAILTRAP_USER, MAILTRAP_PASS } from "#/utils/variables";
+import { generateToken } from "#/utils/helper";
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { name, email, password } = req.body;
 
   const user = await User.create({ name, email, password });
 
-  // Send verification email
+  const token = generateToken();
+  await emailVerificationToken.create({
+    owner: user._id,
+    token,
+  });
+
   const transport = nodemailer.createTransport({
     host: "sandbox.smtp.mailtrap.io",
     port: 2525,
@@ -18,10 +25,11 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
       pass: MAILTRAP_PASS,
     },
   });
+
   transport.sendMail({
     to: user.email,
     from: "auth@myapp.com",
-    html: "<h1>Podify Verification</h1>",
+    html: `<h1>Your verification token is: ${token}</h1>,`,
   });
   res.status(201).json({ user });
 };
