99e13047434f9a45f47fcac6f32879e0ec1eca05
Stephen Moore <judo2000@comcast.net>
2023-09-30 14:16:42 -0600

    Create email template

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,11 +1,13 @@
 import { RequestHandler } from "express";
 import nodemailer from "nodemailer";
+import path from "path";
 
 import User from "#/models/user";
 import emailVerificationToken from "#/models/emailVerificationToken";
 import { CreateUser } from "#/@types/user";
 import { MAILTRAP_USER, MAILTRAP_PASS } from "#/utils/variables";
 import { generateToken } from "#/utils/helper";
+import { generateTemplate } from "#/mail/template";
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { name, email, password } = req.body;
 
@@ -26,10 +28,32 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
     },
   });
 
+  const welcomeMessage = `Hi ${name}, welcome to Podify!  There are so many things we do for verified users.  Use the given OTP (One-Time-Password) to verify your email.`;
+
   transport.sendMail({
     to: user.email,
     from: "auth@myapp.com",
-    html: `<h1>Your verification token is: ${token}</h1>,`,
+    subject: "Welcome to Podify",
+    html: generateTemplate({
+      title: "Welcome to Podify",
+      message: welcomeMessage,
+      logo: "cid:logo",
+      banner: "cid:welcome",
+      link: "#",
+      btnTitle: token,
+    }),
+    attachments: [
+      {
+        filename: "logo.png",
+        path: path.join(__dirname, "../mail/logo.png"),
+        cid: "logo",
+      },
+      {
+        filename: "welcome.png",
+        path: path.join(__dirname, "../mail/welcome.png"),
+        cid: "welcome",
+      },
+    ],
   });
   res.status(201).json({ user });
 };
