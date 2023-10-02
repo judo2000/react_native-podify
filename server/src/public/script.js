88b705dd4958e18f89e1fb8ce2f897e1c300e033
Stephen Moore <judo2000@comcast.net>
2023-10-02 17:32:35 -0600

    Complete functions to update password

--- server/src/public/script.js
+++ server/src/public/script.js
@@ -16,6 +16,8 @@ success.style.display = "none";
 container.style.display = "none";
 
 let token, userId;
+const passRegex =
+  /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])[a-zA-Z\d!@#\$%\^&\*]+$/;
 
 // get query params i.e. token and id
 window.addEventListener("DOMContentLoaded", async () => {
@@ -27,7 +29,7 @@ window.addEventListener("DOMContentLoaded", async () => {
   console.log(params);
   token = params.token;
   userId = params.userId;
-  console.log(token, userId);
+
   const res = await fetch("/auth/verify-pass-reset-token", {
     method: "POST",
     headers: {
@@ -48,3 +50,66 @@ window.addEventListener("DOMContentLoaded", async () => {
   loader.style.display = "none";
   container.style.display = "block";
 });
+
+const displayError = (errorMessage) => {
+  success.style.display = "none";
+  error.innerText = errorMessage;
+  error.style.display = "block";
+};
+
+const displaySuccess = (successMessage) => {
+  error.style.display = "none";
+  success.innerText = successMessage;
+  success.style.display = "block";
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  // validate
+  if (!password.value.trim()) {
+    // render error
+    return displayError("Password is missing!");
+  }
+
+  if (!passRegex.test(password.value)) {
+    // render error
+    console.log("They to not match");
+    return displayError(
+      "Password is too simple.  It must contain alpha numerice and special characters!"
+    );
+  }
+
+  if (password.value !== confirmPassword.value) {
+    // render error
+    return displayError("Passwords do not match!");
+  }
+
+  button.disabled = true;
+  button.innerText = "Please wait...";
+
+  // handle submit
+  const res = await fetch("/auth/update-password", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({
+      token,
+      userId,
+      password: password.value,
+    }),
+  });
+  button.disabled = false;
+  button.innerText = "Reset Password";
+
+  if (!res.ok) {
+    const { error } = await res.json();
+    return displayError(error);
+  }
+  displaySuccess("Your password has been successfully reset!");
+  password.value = "";
+  confirmPassword.value = "";
+};
+
+form.addEventListener("submit", handleSubmit);
