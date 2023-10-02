904d3a4f793da65adf36f1fc851a55db744f30e9
Stephen Moore <judo2000@comcast.net>
2023-10-02 13:33:00 -0600

    Create public folder and server from backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/auth", authRouter);
+app.use(express.static("src/public"));
 
 const PORT = 8989;
 app.listen(PORT, () => {
