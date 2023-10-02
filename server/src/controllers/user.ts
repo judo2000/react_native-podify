8193683d9c25bda290f6db0176c1e9a3110b4f9e
Stephen Moore <judo2000@comcast.net>
2023-10-01 19:46:43 -0600

    Create validation middleware

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -109,20 +109,6 @@ export const generateForgetPasswordLink: RequestHandler = async (req, res) => {
   res.json({ message: "Check your email!" });
 };
 
-export const isValidPassResetToken: RequestHandler = async (req, res) => {
-  const { token, userId } = req.body;
-
-  const resetToken = await PasswordResetToken.findOne({ owner: userId });
-  if (!resetToken)
-    return res
-      .status(403)
-      .json({ error: "Unauthorized access, invalid token!" });
-
-  const matched = await resetToken.compareToken(token);
-  if (!matched)
-    return res
-      .status(403)
-      .json({ error: "Unauthorized access, invalid token!" });
-
-  res.json({ message: "your token is valid" });
+export const grantValid: RequestHandler = async (req, res) => {
+  res.json({ valid: true });
 };
