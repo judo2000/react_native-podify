5da5592d0d32ae17c8977bda16beb3d206cbe967
Stephen Moore <judo2000@comcast.net>
2023-10-01 18:41:14 -0600

    Create generatePasswordLink route and controller

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -6,6 +6,9 @@ import { CreateUser, VerifyEmailRequest } from "#/@types/user";
 import { sendVerificationMail } from "#/utils/mail";
 import { generateToken } from "#/utils/helper";
 import EmailVerificationToken from "#/models/emailVerificationToken";
+import PasswordResetToken from "#/models/passwordResetToken";
+import crypto from "crypto";
+import { PASSWORD_RESET_LINK } from "#/utils/variables";
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { name, email, password } = req.body;
@@ -76,4 +79,26 @@ export const sendReVerificationToken: RequestHandler = async (req, res) => {
   });
 
   res.json({ message: "Please check your email!" });
+};
+
+export const generateForgetPasswordLink: RequestHandler = async (req, res) => {
+  const { email } = req.body;
+  console.log(email);
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(404).json({ error: "Account not found!" });
+
+  // generate reset password link
+
+  // create token using crypto
+  const token = crypto.randomBytes(36).toString("hex");
+
+  await PasswordResetToken.create({
+    owner: user._id,
+    token,
+  });
+
+  const resetLink = `${PASSWORD_RESET_LINK}?token=${token}&id=${user._id}`;
+
+  res.json({ resetLink });
 };
