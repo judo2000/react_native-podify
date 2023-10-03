45cf723ac6dbfc4c80ebcd6145d9598cc1a6ab73
Stephen Moore <judo2000@comcast.net>
2023-10-03 15:41:42 -0600

    Upload file to local directory

--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -46,16 +46,30 @@ router.get("/is-auth", mustAuth, (req, res) => {
 });
 
 import formidable from "formidable";
+import path from "path";
+import fs from "fs";
 
-router.post("/update-profile", (req, res) => {
+router.post("/update-profile", async (req, res) => {
   if (!req.headers["content-type"]?.startsWith("multipart/form-data;"))
     return res.status(422).json({ error: "Only accepts form-data" });
 
   // handle file upload
-  const form = formidable();
+  const dir = path.join(__dirname, "../public/profiles");
+
+  try {
+    await fs.readdirSync(dir);
+  } catch (error) {
+    await fs.mkdirSync(dir);
+  }
+  const form = formidable({
+    uploadDir: dir,
+    filename(name, ext, part, form) {
+      return Date.now() + "_" + part.originalFilename;
+    },
+  });
   form.parse(req, (err, fields, files) => {
-    console.log("Fields", fields);
-    console.log("Files", files);
+    // console.log("Fields", fields);
+    // console.log("Files", files);
 
     res.json({ uploaded: true });
   });
