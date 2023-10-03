297fba9cced43f27cec730ec56b0af176ab18496
Stephen Moore <judo2000@comcast.net>
2023-10-03 16:04:06 -0600

    Create fileParser middleware

--- server/file-parser-middleware/src/routers/auth.ts
+++ server/file-parser-middleware/src/routers/auth.ts
@@ -11,13 +11,11 @@ import { isValidPassResetToken, mustAuth } from "#/middleware/auth";
 import { validate } from "#/middleware/validator";
 import {
   CreateUserSchema,
-  SignValidationSchema,
+  SignInValidationSchema,
   TokenAndIDValidation,
   UpdatePasswordSchema,
 } from "#/utils/validationSchema";
-import { JWT_SECRET } from "#/utils/variables";
 import { Router } from "express";
-import { JwtPayload, verify } from "jsonwebtoken";
 import fileParser, { RequestWithFiles } from "#/middleware/fileParser";
 
 const router = Router();
@@ -38,7 +36,7 @@ router.post(
   isValidPassResetToken,
   updatePassword
 );
-router.post("/sign-in", validate(SignValidationSchema), signIn);
+router.post("/sign-in", validate(SignInValidationSchema), signIn);
 router.get("/is-auth", mustAuth, (req, res) => {
   res.json({
     profile: req.user,
---
+++ server/file-parser-middleware/src/utils/variables.ts
@@ -0,0 +1,11 @@
+const { env } = process as { env: { [key: string]: string } };
+
+export const {
+  MONGO_URI,
+  MAILTRAP_USER,
+  MAILTRAP_PASS,
+  VERIFICATION_EMAIL,
+  PASSWORD_RESET_LINK,
+  SIGN_IN_URL,
+  JWT_SECRET,
+} = env;
