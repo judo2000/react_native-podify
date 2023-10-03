fd08841d40964d1e321c633f7657ef312b030b79
Stephen Moore <judo2000@comcast.net>
2023-10-03 14:58:40 -0600

    Install formidable for file upload and create update-profile route

--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -44,4 +44,20 @@ router.get("/is-auth", mustAuth, (req, res) => {
     profile: req.user,
   });
 });
+
+import formidable from "formidable";
+
+router.post("/update-profile", (req, res) => {
+  if (!req.headers["content-type"]?.startsWith("multipart/form-data;"))
+    return res.status(422).json({ error: "Only accepts form-data" });
+
+  // handle file upload
+  const form = formidable();
+  form.parse(req, (err, fields, files) => {
+    console.log("Fields", fields);
+    console.log("Files", files);
+
+    res.json({ uploaded: true });
+  });
+});
 export default router;
