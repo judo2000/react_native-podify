8806a95022f57617dc5de3182e2cc4e66afe4733
Stephen Moore <judo2000@comcast.net>
2023-10-04 15:40:06 -0600

    Create isVerified middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -47,3 +47,9 @@ export const mustAuth: RequestHandler = async (req, res, next) => {
 
   next();
 };
+
+export const isVerified: RequestHandler = (req, res, next) => {
+  if (!req.user.verified)
+    return res.status(403).json({ error: "Please verify your email address." });
+  next();
+};
