b6cb92949e2949287493d683cd723b9888280dc1
Stephen Moore <judo2000@comcast.net>
2023-10-05 17:26:24 -0600

    Create toggleFavorite controller

--- server/src/controllers/favorite.ts
+++ server/src/controllers/favorite.ts
@@ -1,3 +1,51 @@
 import { RequestHandler } from "express";
+import { isValidObjectId } from "mongoose";
+import Audio from "#/models/audio";
+import Favorite from "#/models/favorites";
 
-export const toggleFavorite: RequestHandler = async (req, res) => {};
+export const toggleFavorite: RequestHandler = async (req, res) => {
+  const audioId = req.query.audioId;
+  let status: "added" | "removed";
+
+  if (!isValidObjectId(audioId))
+    return res.status(422).json({ error: "Audio id is invalid" });
+  const audio = await Audio.findById(audioId);
+
+  if (!audio) return res.status(404).json({ error: "Resource not found!" });
+
+  // if audioId is already in favorites
+  const alreadyExists = await Favorite.findOne({
+    owner: req.user.id,
+    items: audioId,
+  });
+
+  if (alreadyExists) {
+    await Favorite.updateOne(
+      { owner: req.user.id },
+      {
+        $pull: { items: audioId },
+      }
+    );
+
+    status = "removed";
+  } else {
+    const favorite = await Favorite.findOne({ owner: req.user.id });
+    if (favorite) {
+      // trying to update old fav list
+      await Favorite.updateOne(
+        { owner: req.user.id },
+        {
+          // addToSet will not allow dupliate items so it will
+          // add the audioId only if it is not already in there
+          $addToSet: { items: audioId },
+        }
+      );
+    } else {
+      // trying to create a new fav list
+      await Favorite.create({ owner: req.user.id, items: [audioId] });
+    }
+
+    status = "added";
+  }
+  res.json({ status });
+};
