8719a5bb6451d1d60d0fae8b854d9569d971721b
Stephen Moore <judo2000@comcast.net>
2023-10-06 15:27:15 -0600

    Create get all uploads for an authanticated user

--- server/src/controllers/profile.ts
+++ server/src/controllers/profile.ts
@@ -1,4 +1,6 @@
+import { paginationQuery } from "#/@types/misc";
 import User from "#/models/user";
+import Audio from "#/models/audio";
 import { RequestHandler } from "express";
 import { isValidObjectId } from "mongoose";
 
@@ -58,4 +60,26 @@ export const updateFollower: RequestHandler = async (req, res) => {
     );
   }
   res.json({ status });
+};
+
+export const getUploads: RequestHandler = async (req, res) => {
+  const { pageNo = "0", limit = "20" } = req.query as paginationQuery;
+
+  const data = await Audio.find({ owner: req.user.id })
+    .skip(parseInt(limit) * parseInt(pageNo))
+    .limit(parseInt(limit))
+    .sort("-createdAt");
+
+  const audios = data.map((item) => {
+    return {
+      id: item._id,
+      title: item.title,
+      about: item.about,
+      file: item.file.url,
+      poster: item.poster?.url,
+      date: item.createdAt,
+      owner: { name: req.user.name, id: req.user.id },
+    };
+  });
+  res.json({ audios });
 };
