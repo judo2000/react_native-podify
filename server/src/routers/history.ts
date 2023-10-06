51c6eb918e012e5c8631755cd5f1b82141d2b6ad
Stephen Moore <judo2000@comcast.net>
2023-10-06 17:45:25 -0600

    Create Update History Validation Schema

--- server/src/routers/history.ts
+++ server/src/routers/history.ts
@@ -1,9 +1,16 @@
 import { updateHistory } from "#/controllers/history";
 import { mustAuth } from "#/middleware/auth";
+import { validate } from "#/middleware/validator";
+import { UpdateHistoryValidationSchema } from "#/utils/validationSchema";
 import { Router } from "express";
 
 const router = Router();
 
-router.post("/", mustAuth, updateHistory);
+router.post(
+  "/",
+  mustAuth,
+  validate(UpdateHistoryValidationSchema),
+  updateHistory
+);
 
 export default router;
