f52fbf4872facffb257e1dd4e5a8dd173bc9c8bb
Stephen Moore <judo2000@comcast.net>
2023-10-06 00:01:44 -0600

    Create Playlist model

---
+++ server/src/models/playlist.ts
@@ -0,0 +1,42 @@
+import { Model, ObjectId, Schema, model, models } from "mongoose";
+
+interface PlaylistDocument {
+  title: string;
+  owner: ObjectId;
+  items: ObjectId[];
+  visibility: "public" | "private" | "auto";
+}
+
+const playlistSchema = new Schema<PlaylistDocument>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: String,
+      required: true,
+      ref: "User",
+    },
+    items: [
+      {
+        type: String,
+        required: true,
+        ref: "Audio",
+      },
+    ],
+    visibility: {
+      type: String,
+      enum: ["public", "private", "tual"],
+      default: "public",
+    },
+  },
+  { timestamps: true }
+);
+
+// below is anotehr way of doing
+// export default model("Playlist", playlistSchema) as Model<PlaylistDocument>;
+
+const Playlist = models.Playlist || model("Playlist", playlistSchema);
+
+export default Playlist as Model<PlaylistDocument>;
