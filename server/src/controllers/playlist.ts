544614b9a6f4ba68d8020a872641dc1ba37c72bf
Stephen Moore <judo2000@comcast.net>
2023-10-06 11:20:00 -0600

    Build Create Playlist functionality

--- server/src/controllers/playlist.ts
+++ server/src/controllers/playlist.ts
@@ -1,5 +1,32 @@
+import { CreatePlaylistRequest } from "#/@types/audio";
+import Audio from "#/models/audio";
+import Playlist from "#/models/playlist";
 import { RequestHandler } from "express";
 
-export const createPlaylist: RequestHandler = (req, res) => {
-  d;
+export const createPlaylist: RequestHandler = async (
+  req: CreatePlaylistRequest,
+  res
+) => {
+  const { title, resId, visibility } = req.body;
+  const ownerId = req.user.id;
+
+  if (resId) {
+    const audio = await Audio.findById(resId);
+    if (!audio) return res.status(404).json({ error: "Could not find audio!" });
+  }
+  const newPlaylist = new Playlist({
+    title,
+    owner: ownerId,
+    visibility,
+  });
+
+  if (resId) newPlaylist.items = [resId as any];
+  await newPlaylist.save();
+  res.status(201).json({
+    playlist: {
+      id: newPlaylist._id,
+      title: newPlaylist.title,
+      visibility: newPlaylist.visibility,
+    },
+  });
 };
